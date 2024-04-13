346f921027796bc580c22474cc6e0f208e8c727e
noname_d <davletbaev-timur@mail.ru>
2024-04-13 20:00:23 +0300

    initial

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -9,11 +9,68 @@ import Step6 from "./steps/Step6";
 import Step7 from "./steps/Step7";
 import Step8 from "./steps/Step8";
 import {useTelegram} from "../../hooks/useTelegram";
+import {CircularProgress} from "@mui/material";
 
 const Main = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const preloadImages = (srcArray) => {
+        srcArray.forEach((src) => {
+            const img = new Image();
+            img.src = src;
+        }).then(() => setIsLoading(false));
+    };
+
+    useEffect(() => {
+        const images = [
+            '../../assets/main/step8/hand.png',
+            '../../assets/main/step7/wa.svg',
+            '../../assets/main/step7/twomoney.png',
+            '../../assets/main/step7/trofey.png',
+            '../../assets/main/step7/tg.svg',
+            '../../assets/main/step7/phone.svg',
+            '../../assets/main/step7/onemoney.png',
+            '../../assets/main/step7/money.png',
+            '../../assets/main/step6/phone.png',
+            '../../assets/main/step6/folder.png',
+            '../../assets/main/step6/clock.png',
+            '../../assets/main/step6/bell.png',
+            '../../assets/main/step5/wb.svg',
+            '../../assets/main/step5/coinmarketcap.svg',
+            '../../assets/main/step5/mount.svg',
+            '../../assets/main/step5/gosuslugi.svg',
+            '../../assets/main/step5/bitrix.svg',
+            '../../assets/main/step5/onec.svg',
+            '../../assets/main/step5/ozon.svg',
+            '../../assets/main/step5/crm.svg',
+            '../../assets/main/step4/mark.svg',
+            '../../assets/main/step4/hand.png',
+            '../../assets/main/step4/car.png',
+            '../../assets/main/step3/youtube.png',
+            '../../assets/main/step3/mark.png',
+            '../../assets/main/step3/clock.png',
+            '../../assets/main/step3/folder.png',
+            '../../assets/main/step2/store.png',
+            '../../assets/main/step2/phone.png',
+            '../../assets/main/step2/microsoft.png',
+            '../../assets/main/step2/google.png',
+            '../../assets/main/step2/paper.png',
+            '../../assets/main/step2/chat.png',
+            '../../assets/main/step1/wifi.png',
+            '../../assets/main/step1/clock.png',
+            '../../assets/main/step1/card.png',
+            '../../assets/main/step1/note.png',
+            '../../assets/main/step1/phone.png',
+            '../../assets/main/step1/bottom_ellipse.png',
+            '../../assets/main/step1/top_ellipse.png'
+        ];
+
+        preloadImages(images);
+    }, []);
+
+
     const {tg} = useTelegram();
     const [currentPage, setCurrentPage] = useState(1);
-    const [history, setHistory] = useState([1])
+    const [history, setHistory] = useState([1]);
 
     const goToStep = (page) => {
         setHistory([...history, page]);
@@ -71,7 +128,12 @@ const Main = () => {
             minHeight: "100vh",
         }}>
             <div style={{maxHeight: "calc(100svh - 87px)", height: "calc(100svh - 87px)"}}>
-                {renderCurrentPage()}
+                {isLoading ?
+                    <div style={{height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        <CircularProgress/>
+                    </div> :
+                    renderCurrentPage()
+                }
             </div>
         </div>
     );
